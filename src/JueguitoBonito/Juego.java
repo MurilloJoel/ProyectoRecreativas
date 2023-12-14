35dfb68d0dc7895206c20193de509c1f3b50fc7a
developer <developer@68da2f2f2e33>
2023-12-14 13:11:04 +0000

    Cuarto commit del Juego y la Tarjeta

--- src/JueguitoBonito/Juego.java
+++ src/JueguitoBonito/Juego.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package JueguitoBonito;
 
 /**
  *
- * @author developer
- */
-public class Juego {
-    
+ 
+
+    @author Joel Murillo 
+        @author Patricia Jiménez (◉Θ◉)
+        @author Leonor Machicado
+    */
+
+public class Juego 
+{
+    //VARIABLES
+    private String nombre; //nombre del juego
+    private int coste; //el coste del juego
+
+    //CONSTRUCTOR
+
+    public Juego(String nombre, int coste) 
+    {
+        this.nombre = nombre;
+        this.coste = coste;
+    }
+
+    //MÉTODO JUGAR
+    public int jugar (Tarjeta t)
+    {
+        //hace comprobaciones y saca mensajes
+        if()
+        {
+
+        }
+        int tickets = 1+(int)(Math.random()*20); //genera tickets del 1 al 20
+
+
+        return tickets;
+    }
+
+    //GETTER
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCoste() {
+        return coste;
+    }
+
+    //SETTER
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCoste(int coste) {
+        this.coste = coste;
+    }
+
+
+
+
+
+
+
 }
--- src/JueguitoBonito/Tarjeta.java
+++ src/JueguitoBonito/Tarjeta.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package JueguitoBonito;
-
 /**
  *
- * @author joel patricia y leonor
- */
-public class Tarjeta {
-    
+ 
+
+    @author Joel Murillo
+        @author Patricia Jiménez
+        @author Leonor Machicado
+    */
+
+public class Tarjeta 
+{
+    //VARIABLES DE INSTANCIA
+    private int creditos; //creditos de la tarjeta
+    private int ticket; //tickets que da la máquina
+    private int numtarj; //el número de la tarjeta
+    // ATRIBUTOS DE CLASE
+    static private int contador = 1; //contador nº tarjeta autoincrementado 
+    // (es un contador que cuando se hace una nueva tarjeta aumenta 1 en el contador)
+
+    //CONSTRUCTOR
+    public Tarjeta(int creditos) 
+    {
+        this.creditos = creditos;
+        this.ticket = 0;
+        this.numtarj = contador;
+        contador++;
+    }
+
+    //METODOS GETTER
+
+    public int getCreditos() 
+    {
+        return creditos;
+    }
+
+    public int getTicket() 
+    {
+        return ticket;
+    }
+
+    public int getNumtarj() 
+    {
+        return numtarj;
+    }
+
+
+    //METODOS SETTER
+
+    public void setCreditos(int creditos) 
+    {
+        this.creditos = creditos;
+    }
+
+    public void setTicket(int ticket) 
+    {
+        this.ticket = ticket;
+    }
+
 }
