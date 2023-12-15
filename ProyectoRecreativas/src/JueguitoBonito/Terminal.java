fa2fd3be8bd12e2101422350d24fe6924291165f
joel <joel@DESKTOP-BO0U0VT>
2023-12-15 19:02:15 +0100

    commit del terminal

---
+++ ProyectoRecreativas/src/JueguitoBonito/Terminal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JueguitoBonito;
+
+/**
+        @author Joel Murillo
+        @author Patricia Jiménez
+        @author Leonor Machicado
+ */
+public class Terminal {
+    
+}
