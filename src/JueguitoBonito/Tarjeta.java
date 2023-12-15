b9b5f200551ac9b8e849bda8a7db3b3fd77784f5
patriciajb02 <148756852+patriciajb02@users.noreply.github.com>
2023-12-15 19:00:42 +0100

    Quinto commit Tarjeta, acabao?

--- src/JueguitoBonito/Tarjeta.java
+++ src/JueguitoBonito/Tarjeta.java
@@ -3,10 +3,8 @@ package JueguitoBonito;
 
 /**
     *
- 
-
     @author Joel Murillo
-        @author Patricia Jiménez
+        @author Patricia Jiménez (◉Θ◉)
         @author Leonor Machicado
     */
 
