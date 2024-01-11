c76b873daa6335c24a89cbef86058d81c93deb4d
LeonorMC <151014971+LeonorMC@users.noreply.github.com>
2024-01-11 20:35:53 +0100

    Update Premios.java
    último commit Premios

--- src/JueguitoBonito/Premios.java
+++ src/JueguitoBonito/Premios.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package JueguitoBonito;
 
 /**
- *
- * @author developer
- */
-public class Premios {
+    @author Joel Murillo
+    @author Patricia Jiménez
+    @author Leonor Machicado
+    */
+
+public class Premio{
     
+    //atributos
+    
+    private String premio;
+    private int precio;
+    private int stock;
+
+    //getters
+    
+    public String getPremio(){
+        return premio;
+    }
+
+    public int getStock(){
+        return stock;
+    }
+    
+    public int getPrecio(){
+        return precio;
+    }
+    
+    //setters
+    
+    public void setPremio(String premio){
+        this.premio = premio;
+    }
+    
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+    
+    @Override
+    public String toString(){
+        return "Premio{" + "premio=" + premio + ", precio=" + precio + ", stock=" + stock + '}';
+    }
+    
+    public Premio(String premio, int precio, int stock){
+        this.premio = premio;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
 }
